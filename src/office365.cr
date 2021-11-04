d541d86a8f2d24fe7b9ac77f12bf41474e0f43bc
tassja <xtassja@gmail.com>
2021-11-04 16:29:37 +1100

    feat(office365): init with scopes (#16)
    * feat(office365): init with scopes
    
    * chore(shard): bump version
    
    * refactor(office365): simpler getter
    
    Co-authored-by: Caspian Baska <caspian@place.tech>

--- src/office365.cr
+++ src/office365.cr
@@ -3,16 +3,15 @@ require "office365"
 module PlaceCalendar
   class Office365 < Interface
     DEFAULT_CONFERENCE = "teamsForBusiness"
+    DEFAULT_SCOPE      = "https://graph.microsoft.com/.default"
 
-    def initialize(@tenant : String, @client_id : String, @client_secret : String, @conference_type : String? = DEFAULT_CONFERENCE)
+    def initialize(@tenant : String, @client_id : String, @client_secret : String, @conference_type : String? = DEFAULT_CONFERENCE, @scopes : String = DEFAULT_SCOPE)
     end
 
-    def client_id : Symbol
-      :office365
-    end
+    getter client_id : Symbol = :office365
 
     def client : ::Office365::Client
-      @client ||= ::Office365::Client.new(@tenant, @client_id, @client_secret)
+      @client ||= ::Office365::Client.new(@tenant, @client_id, @client_secret, @scopes)
     end
 
     def access_token(user_id : String? = nil) : NamedTuple(expires: Time, token: String)
