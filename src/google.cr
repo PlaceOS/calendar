7d831538e6e24627d5056b7f8a404f1d7a398751
Mia Bennett <mia@chillfox.com>
2023-03-01 13:20:22 +0930

    refactor(event): include hide_attendees in #event_params

--- src/google.cr
+++ src/google.cr
@@ -309,17 +309,18 @@ module PlaceCalendar
 
     private def event_params(event, calendar_id)
       params = {
-        event_start:         event.event_start,
-        event_end:           event.event_end || Time.local + 1.hour,
-        calendar_id:         calendar_id ? calendar_id : "primary",
-        attendees:           event.attendees.map { |e| e.response_status ? {email: e.email, responseStatus: e.response_status} : {email: e.email} },
-        all_day:             event.all_day?,
-        visibility:          event.private? ? ::Google::Visibility::Private : ::Google::Visibility::Default,
-        summary:             event.title,
-        description:         event.body,
-        location:            event.location,
-        recurrence:          nil,
-        extended_properties: event.extended_properties,
+        event_start:                 event.event_start,
+        event_end:                   event.event_end || Time.local + 1.hour,
+        calendar_id:                 calendar_id ? calendar_id : "primary",
+        attendees:                   event.attendees.map { |e| e.response_status ? {email: e.email, responseStatus: e.response_status} : {email: e.email} },
+        guests_can_see_other_guests: !event.hide_attendees,
+        all_day:                     event.all_day?,
+        visibility:                  event.private? ? ::Google::Visibility::Private : ::Google::Visibility::Default,
+        summary:                     event.title,
+        description:                 event.body,
+        location:                    event.location,
+        recurrence:                  nil,
+        extended_properties:         event.extended_properties,
       }
       if event.recurrence
         e_recurrence = event.recurrence.not_nil!
