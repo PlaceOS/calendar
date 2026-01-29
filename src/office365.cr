b084be33e1238a1f77379f3f5449309538e7b3b4
Stephen von Takach <steve@place.technology>
2026-01-29 14:26:17 +1100

    fix(calendars): use default view interval

--- src/office365.cr
+++ src/office365.cr
@@ -427,7 +427,7 @@ module PlaceCalendar
     end
 
     def get_availability(user_id : String, calendars : Array(String), starts_at : Time, ends_at : Time, **options) : Array(AvailabilitySchedule)
-      view_interval = options[:view_interval]? || Office365Availability.select_view_interval(starts_at - ends_at)
+      view_interval = options[:view_interval]? || Office365Availability.select_view_interval(ends_at - starts_at)
 
       # Max is 100 so we need to batch if we're above this
       if calendars.size > 100
